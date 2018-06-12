5a872df6ac91541165acc506b3d0ea203f8cfbed
johnatan.jcr <johnatan.jcr@gmail.com>
2018-06-12 15:25:08 -0400

    novo cabecalho

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Mensagem no cabeçalho.")
+        self.assertRegex(result.data.decode(), "Mensagem")
 
 
 if __name__ == "__main__":
